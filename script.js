2160b67f0bdb8626b321a4b7e4ec44ba10137b53
chrishampel88 <christianhampel88gmail.com>
2026-06-28 20:25:09 -0300

    listo

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 
 let cuenta = '';
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.btn1').forEach((btn) => {
+        btn.addEventListener('click', function () {
+            this.classList.remove('btn-pressed');
+            void this.offsetWidth;
+            this.classList.add('btn-pressed');
+
+            setTimeout(() => {
+                this.classList.remove('btn-pressed');
+            }, 80);
+        });
+    });
+});
+
 function boton(number){
     const operadores = ['+', '-', 'x', '/'];
     const ultimo = cuenta[cuenta.length - 1];
@@ -9,17 +24,19 @@ function boton(number){
     }
       cuenta += number;
     $('#resultado').val(cuenta);
-}
 
+
+}
+let resultado='';
 function calcular(){
     
-    let resultado = eval(cuenta);
+    resultado = eval(cuenta);
     $('#resultado').val(resultado);
-    cuenta=resultado
+    cuenta=resultado;
 }
 
 function limpiar(){
-    
-    cuenta=0
+    cuenta=0;
+    resultado='';
     $('#resultado').val(0);
 }
